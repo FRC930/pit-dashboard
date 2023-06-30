dd07af10111690780683f54a830ebfbb94200e2c
Greg Billetdeaux <greg@ionic.io>
2023-06-30 14:25:34 -0500

    Added suffixes to estimated times

--- src/app/event/event-schedule-component/event-schedule-component.component.ts
+++ src/app/event/event-schedule-component/event-schedule-component.component.ts
@@ -6,22 +6,25 @@ import formatDistance from 'date-fns/formatDistance';
 @Component({
   selector: 'app-event-schedule-component',
   templateUrl: './event-schedule-component.component.html',
-  styleUrls: ['./event-schedule-component.component.css']
+  styleUrls: ['./event-schedule-component.component.css'],
 })
 export class EventScheduleComponentComponent {
   @Input() matches: TBAMatch[] = [];
 
   matchNumber(match: TBAMatch): string {
     if (match.comp_level === 'sf') return `SF${match.set_number}`;
-    return `${match.comp_level}${match.match_number}`.toUpperCase(); 
+    return `${match.comp_level}${match.match_number}`.toUpperCase();
   }
 
   allianceString(teamKeys: string[]): string {
-    return teamKeys.map(k => k.replace('frc', '')).join(' ');
+    return teamKeys.map((k) => k.replace('frc', '')).join(' ');
   }
 
   timeToMatch(match: TBAMatch): string {
-    if(match.predicted_time) return formatDistance(new Date(match.predicted_time*1000), new Date())
+    if (match.predicted_time)
+      return formatDistance(new Date(match.predicted_time * 1000), new Date(), {
+        addSuffix: true,
+      });
     return '';
   }
 }
