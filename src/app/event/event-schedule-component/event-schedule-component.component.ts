5776eae29d7120874e2bf0d6294d906bae2f8668
Greg Billetdeaux <greg@ionic.io>
2023-06-30 15:51:19 -0500

    Added delayd schedule logic

--- src/app/event/event-schedule-component/event-schedule-component.component.ts
+++ src/app/event/event-schedule-component/event-schedule-component.component.ts
@@ -1,15 +1,37 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { TBAMatch } from '@frc930/frc-data-lib';
 
 import formatDistance from 'date-fns/formatDistance';
+import differenceInMinutes from 'date-fns/differenceInMinutes';
 
 @Component({
   selector: 'app-event-schedule-component',
   templateUrl: './event-schedule-component.component.html',
   styleUrls: ['./event-schedule-component.component.css'],
 })
-export class EventScheduleComponentComponent {
+export class EventScheduleComponentComponent implements OnChanges {
   @Input() matches: TBAMatch[] = [];
+  private delay = 0;
+
+  ngOnChanges() {
+    if (this.matches.length > 0) {
+      const nextMatch = this.matches[0];
+      if (nextMatch.predicted_time) {
+        const difference = differenceInMinutes(
+          new Date(nextMatch.predicted_time * 1000),
+          new Date(),
+          { roundingMethod: 'floor' }
+        );
+        this.delay = Math.abs(Math.min(0, difference));
+      }
+    }
+  }
 
   matchNumber(match: TBAMatch): string {
     if (match.comp_level === 'sf') return `SF${match.set_number}`;
@@ -21,10 +43,16 @@ export class EventScheduleComponentComponent {
   }
 
   timeToMatch(match: TBAMatch): string {
-    if (match.predicted_time)
-      return formatDistance(new Date(match.predicted_time * 1000), new Date(), {
-        addSuffix: true,
-      });
+    if (match.predicted_time) {
+      console.log(this.delay);
+      return formatDistance(
+        new Date((match.predicted_time + this.delay * 60) * 1000),
+        new Date(),
+        {
+          addSuffix: true,
+        }
+      );
+    }
     return '';
   }
 }
